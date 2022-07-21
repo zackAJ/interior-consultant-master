d01225cd1b9a9bb572e88b0cb5cb5bfaffa8b2bc
zackAJ <aktouf98zakaria@gmail.com>
2022-07-21 01:43:02 +0100

    done

---
+++ script.js
@@ -0,0 +1,22 @@
+const burger = document.getElementById("burger");
+const nav = document.getElementById("navigation");
+const ul = document.getElementById("list");
+const bar1 = document.getElementsByClassName('bar1')[0];
+const bar2 = document.getElementsByClassName('bar2')[0];
+const bar3 = document.getElementsByClassName('bar3')[0];
+const links = document.getElementsByTagName("a");
+burger.onclick = () => {
+  nav.classList.toggle('navOn');
+  ul.classList.toggle('ulOn');
+  bar1.classList.toggle('bar1-animation');
+  bar2.classList.toggle('bar2-animation');
+  bar3.classList.toggle('bar3-animation');
+}
+for (var link of links) {
+  
+    if (link['href'] == location.href) {
+      link.classList.add("underline");
+    }
+    else { link.classList.remove("underline"); }
+  
+}
